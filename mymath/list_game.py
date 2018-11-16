360f46adae92391017d3aa40298d6f1fc36d4f92
Ivana Kralova <ivana.kralova.15@gmail.com>
2018-11-16 19:13:25 +0100

    ADD division of elements in list

--- mymath/list_game.py
+++ mymath/list_game.py
@@ -1,5 +1,4 @@
 a = [10,2,5]
-print(a)
 
 sum_of_list = a[0] + a[1] + a[2]
 print(sum_of_list)
@@ -8,4 +7,14 @@ sum_of_list2 = 0
 for i in a:
     sum_of_list2 += i
 
-print(sum_of_list2)     
+print(sum_of_list2)
+
+
+div_of_list = a[0] * a[0]
+for i in a:
+    if i == 0:
+        print("nulou nelze dělit")
+    else:
+        div_of_list /= i
+
+print(div_of_list)
