328501ba0e5ab744bd90f0e7a967f28e9ef953d8
Ivana Kralova <ivana.kralova.15@gmail.com>
2019-01-14 16:57:02 +0100

    NEW - cube

---
+++ krychle_povrch_obsah.py
@@ -0,0 +1,6 @@
+strana = int(input("Zadej stranu krychle v centimetrech: "))
+ # v centimetrech
+povrch = 6*strana*strana
+objem = strana * strana * strana
+print ("Povrch krychle je:", povrch, " cm")
+print ("Objem krychle je:", objem, "cm")
